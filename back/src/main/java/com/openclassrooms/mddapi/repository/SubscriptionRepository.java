deb80300ea51c899b65dd33330386c2516531ef4
gordinir67 <b.lucas597@laposte.net>
2026-02-08 07:08:45 +0100

    Ajout des repositories

---
+++ back/src/main/java/com/openclassrooms/mddapi/repository/SubscriptionRepository.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.mddapi.repository;
+
+import com.openclassrooms.mddapi.models.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+}
