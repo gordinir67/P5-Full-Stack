6ecb0ef57d76f4e9d98b3647e47e2fd1c6ac3ba7
gordinir67 <b.lucas597@laposte.net>
2026-02-19 18:37:14 +0100

    Ajout articles et modif css (maquette)

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-articles',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    RouterLink,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss'
 })
