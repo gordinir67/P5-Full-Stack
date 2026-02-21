6e40b73b1e4343b5dc37aa8ab5b1a3a6908de950
gordinir67 <b.lucas597@laposte.net>
2026-02-21 13:39:35 +0100

    Ajout theme (maquette)

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
+import { ThemesComponent } from './pages/themes/themes.component';
 
 export const routes: Routes = [
       { path: '', component: HomeComponent },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },  
-      { path: 'articles', component: ArticlesComponent },        
+      { path: 'articles', component: ArticlesComponent },   
+      { path: 'themes', component: ThemesComponent },        
       { path: '**', redirectTo: '' },      
 ];
