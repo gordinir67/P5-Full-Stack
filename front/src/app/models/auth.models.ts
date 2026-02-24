f4724f6733caceb20b6de8be4a00de431426afa6
gordinir67 <b.lucas597@laposte.net>
2026-02-24 15:44:19 +0100

    Creation AuthModels

---
+++ front/src/app/models/auth.models.ts
@@ -0,0 +1,14 @@
+export interface AuthResponse {
+  token: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  email: string;
+  name: string;
+  password: string;
+}
