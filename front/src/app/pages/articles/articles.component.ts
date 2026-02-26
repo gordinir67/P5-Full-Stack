088cc89ad3f369d0a814cd50debe1895d3806e85
gordinir67 <b.lucas597@laposte.net>
2026-02-27 00:40:34 +0100

    Ajout ArticleComponent

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,23 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
+import { ArticleService } from '../../services/article.service';
+import type { ArticleLightDto } from '../../models/article.models';
 
 @Component({
   selector: 'app-articles',
+  standalone: true,
   imports: [
-      HeaderComponent,
-      RouterLink,
-      MatRippleModule,
-      MatCardModule,
+    CommonModule,
+    HeaderComponent,
+    RouterLink,
+    MatRippleModule,
+    MatCardModule,
   ],
   templateUrl: './articles.component.html',
-  styleUrl: './articles.component.scss'
+  styleUrl: './articles.component.scss',
 })
-export class ArticlesComponent {
-onSortDirChange(event: Event) {
+export class ArticlesComponent implements OnInit {
+  articles: ArticleLightDto[] = [];
+  sort: 'asc' | 'desc' = 'desc';
+  isLoading = false;
+  error = '';
 
-}
+  constructor(private readonly articlesApi: ArticleService) {}
 
+  ngOnInit(): void {
+    this.load();
+  }
+
+  onSortDirChange(event: Event): void {
+    const checked = (event.target as HTMLInputElement | null)?.checked ?? false;
+    this.sort = checked ? 'asc' : 'desc';
+    this.load();
+  }
+
+  private load(): void {
+    this.error = '';
+    this.isLoading = true;
+
+    this.articlesApi.list(this.sort).subscribe({
+      next: (items) => {
+        this.articles = items;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+        this.error = 'Impossible de charger les articles.';
+      },
+    });
+  }
 }
